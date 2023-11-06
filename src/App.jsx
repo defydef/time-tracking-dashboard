1369756ba3a4676919de05ebd1a06766f65422ae
defy <8841170+defydef@users.noreply.github.com>
2023-11-06 22:29:07 +0700

    Complete ActivityCard layout for mobile display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import ActivityCard from "./ui/ActivityCard";
 import MainCard from "./ui/MainCard";
+import WorkIcon from "./ui/svg/WorkIcon";
+import PlayIcon from "./ui/svg/PlayIcon";
+import StudyIcon from "./ui/svg/StudyIcon";
+import ExerciseIcon from "./ui/svg/ExerciseIcon";
+import SocialIcon from "./ui/svg/SocialIcon";
+import SelfCareIcon from "./ui/svg/SelfCareIcon";
 
 function App() {
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-6 py-20">
       <MainCard />
-      <ActivityCard />
+      <ActivityCard icon={<WorkIcon />} title="Work" bgVariant="work" />
+      <ActivityCard icon={<PlayIcon />} title="Play" bgVariant="play" />
+      <ActivityCard icon={<StudyIcon />} title="Study" bgVariant="study" />
+      <ActivityCard
+        icon={<ExerciseIcon />}
+        title="Exercise"
+        bgVariant="exercise"
+        top="1rem"
+      />
+      <ActivityCard
+        icon={<SocialIcon />}
+        title="Social"
+        bgVariant="social"
+        top="3rem"
+      />
+      <ActivityCard
+        icon={<SelfCareIcon />}
+        title="Self Care"
+        bgVariant="selfcare"
+        top="1rem"
+      />
     </div>
   );
 }
--- src/ui/ActivityCard.jsx
+++ src/ui/ActivityCard.jsx
@@ -1,27 +1,28 @@
-function ActivityCard() {
+function ActivityCard({ icon, title, bgVariant, top = "2.1rem" }) {
   return (
-    <div className="bg-[var(--work)] w-[20.4375rem] rounded-[0.9375rem] flex flex-col justify-end overflow-hidden">
-      <div className="flex justify-end">
-        <svg
-          width="79"
-          height="79"
-          xmlns="http://www.w3.org/2000/svg"
-          className="mt-[-1rem]"
-        >
-          <path
-            d="m18.687 10.43 15.464 30.906c.31.682.743 1.322 1.3 1.88.558.557 1.198.99 1.714 1.217L68.237 59.98 52.484 75.732a8.025 8.025 0 0 1-11.355 0L2.934 37.538a8.025 8.025 0 0 1 0-11.356L18.687 10.43Zm19.345-7.99 10.839 10.838 2.065-2.064a5.845 5.845 0 0 1 8.258 0l8.258 8.259a5.845 5.845 0 0 1 0 8.258l-2.064 2.064 10.839 10.84a8.025 8.025 0 0 1 0 11.355l-4.728 4.728L39.126 40.53a1.963 1.963 0 0 1-.578-.413 1.963 1.963 0 0 1-.413-.578L21.95 7.168l4.728-4.728a8.025 8.025 0 0 1 11.355 0Zm17.033 12.903-2.064 2.065 8.258 8.258 2.064-2.064-8.258-8.259Z"
-            fill="#D96C47"
-            fillRule="nonzero"
-          />
-        </svg>
+    <div
+      className={`bg-[var(--${bgVariant})] w-[20.4375rem] h-[10rem] rounded-[0.9375rem] flex flex-col overflow-hidden justify-end`}
+    >
+      <div className={`flex justify-end relative z-[1] top-[${top}] mr-2`}>
+        {icon}
       </div>
-      <section className="bg-[var(--card-bg)] rounded-[0.9375rem] px-5 py-6 relative mt-[-1.3rem] p-3">
+      <section className="bg-[var(--card-bg)] h-[7.625rem] rounded-[0.9375rem_0.9375rem_0_0] px-5 py-6 z-[2] relative">
         <div className="flex flex-col">
-          <section>
-            <p>Work</p>
+          <section className="flex justify-between items-center">
+            <p className="text-[1.125rem] font-medium">{title}</p>
+            <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z"
+                fill="#BBC0FF"
+                fillRule="evenodd"
+              />
+            </svg>
           </section>
-          <section>
-            <h1>32hrs</h1>
+          <section className="flex justify-between items-center">
+            <h1 className="text-[2rem] font-light">32hrs</h1>
+            <p className="text-[var(--secondary-text)] text-[0.9375rem]">
+              Last Week - 36hrs
+            </p>
           </section>
         </div>
       </section>
