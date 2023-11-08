666682c162b24622b0b395af9f2af4b9772715f9
defy <8841170+defydef@users.noreply.github.com>
2023-11-08 20:53:36 +0700

    Fix grid display of MainCard in desktop display

--- src/ui/MainCard.jsx
+++ src/ui/MainCard.jsx
@@ -1,8 +1,8 @@
 function MainCard() {
   return (
-    <div className="bg-[var(--card-bg)] w-[20.4375rem] sm:w-[15.9375rem] sm:h-max rounded-[0.9375rem] pb-3 flex flex-col gap-3 sm:row-span-full">
-      <section className="bg-[var(--primary)] rounded-[0.9375rem] px-5 py-6">
-        <div className="flex items-center justify-start gap-5">
+    <div className="bg-[var(--card-bg)] w-[20.4375rem] sm:w-[15.9375rem] rounded-[0.9375rem] pb-3 flex flex-col gap-3 sm:row-span-full">
+      <section className="bg-[var(--primary)] rounded-[0.9375rem] px-5 py-6 sm:p-7 sm:h-[22.125rem]">
+        <div className="flex sm:flex-col items-center sm:items-start justify-start gap-5">
           <div className="rounded-full w-[4.5rem] h-[4.5rem] bg-white flex justify-center items-center">
             <img
               className="rounded-full w-16 h-16"
@@ -13,11 +13,13 @@ function MainCard() {
             <p className="text-[0.9375rem] text-[var(--secondary-text)]">
               Report for
             </p>
-            <h1 className="text-[1.5rem] font-light">Jeremy Robson</h1>
+            <h1 className="text-[1.5rem] sm:text-[2.5rem] font-light leading-tight">
+              Jeremy Robson
+            </h1>
           </div>
         </div>
       </section>
-      <section className="flex justify-around items-end text-[var(--secondary-heading)]">
+      <section className="flex sm:flex-col justify-around items-end sm:items-start sm:gap-3 sm:py-3 sm:px-7 text-[var(--secondary-heading)]">
         <p>Daily</p>
         <p className="text-white">Weekly</p>
         <p>Monthly</p>
