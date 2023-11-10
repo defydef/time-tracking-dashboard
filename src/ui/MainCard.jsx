64b860050528f181e9519074c7fadffd3fec3c5c
defy <8841170+defydef@users.noreply.github.com>
2023-11-10 08:56:49 +0700

    Fix image size on desktop display

--- src/ui/MainCard.jsx
+++ src/ui/MainCard.jsx
@@ -5,10 +5,11 @@ function MainCard({ period, setPeriod }) {
     <div className="bg-[var(--card-bg)] w-[20.4375rem] sm:w-[15.9375rem] rounded-[0.9375rem] pb-3 flex flex-col gap-3 sm:row-span-full">
       <section className="bg-[var(--primary)] rounded-[0.9375rem] px-5 py-6 sm:p-7 sm:h-[22.125rem]">
         <div className="flex sm:flex-col items-center sm:items-start justify-start gap-5">
-          <div className="rounded-full w-[4.5rem] h-[4.5rem] bg-white flex justify-center items-center">
+          <div className="rounded-full w-[4.5rem] sm:w-[5.3rem] h-[4.5rem] sm:h-[5.3rem] bg-white flex justify-center items-center">
             <img
-              className="rounded-full w-16 h-16"
+              className="rounded-full w-16 h-16 sm:w-[4.875rem] sm:h-[4.875rem]"
               src="/images/image-jeremy.png"
+              alt="Profile picture"
             />
           </div>
           <div>
