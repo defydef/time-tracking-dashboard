44fbaa6cc4762bb6276f2833f5b7b77cd5654522
defy <8841170+defydef@users.noreply.github.com>
2023-11-10 09:02:00 +0700

    Fix padding on MainCard

--- src/ui/MainCard.jsx
+++ src/ui/MainCard.jsx
@@ -3,8 +3,8 @@ import TimePeriod from "./TimePeriod";
 function MainCard({ period, setPeriod }) {
   return (
     <div className="bg-[var(--card-bg)] w-[20.4375rem] sm:w-[15.9375rem] rounded-[0.9375rem] pb-3 flex flex-col gap-3 sm:row-span-full">
-      <section className="bg-[var(--primary)] rounded-[0.9375rem] px-5 py-6 sm:p-7 sm:h-[22.125rem]">
-        <div className="flex sm:flex-col items-center sm:items-start justify-start gap-5">
+      <section className="bg-[var(--primary)] rounded-[0.9375rem] px-5 py-6 sm:px-10 sm:py-11 sm:h-[22.125rem]">
+        <div className="flex sm:flex-col items-center sm:items-start justify-start gap-5 sm:gap-12">
           <div className="rounded-full w-[4.5rem] sm:w-[5.3rem] h-[4.5rem] sm:h-[5.3rem] bg-white flex justify-center items-center">
             <img
               className="rounded-full w-16 h-16 sm:w-[4.875rem] sm:h-[4.875rem]"
@@ -22,7 +22,7 @@ function MainCard({ period, setPeriod }) {
           </div>
         </div>
       </section>
-      <section className="flex sm:flex-col justify-around items-end sm:items-start sm:gap-3 sm:py-3 sm:px-7 text-[var(--secondary-heading)] ">
+      <section className="flex sm:flex-col justify-around items-end sm:items-start sm:gap-3 sm:py-3 sm:px-10 text-[var(--secondary-heading)]">
         <TimePeriod
           onSelect={setPeriod}
           label="Daily"
